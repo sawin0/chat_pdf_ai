a9029fe63c2345fc06553b945801c4c30ebcad9e
Sabin Ranabhat <binranabhat@gmail.com>
2026-03-10 12:50:13 +0545

    feat: implement post API for processing pdf

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from app.pdf_downloader import download_pdf
+from app.pdf_text_extractor import extract_text_from_pdf
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+class PDFReuqest(BaseModel):
+    url: str
+
+
+@app.post("/process-pdf")
+def process_pdf(data: PDFReuqest):
+
+    pdf_path = download_pdf(data.url)
+
+    text = extract_text_from_pdf(pdf_path)
+
+    return {"status": "Success", "characters_extracterd(length)": len(text)}
