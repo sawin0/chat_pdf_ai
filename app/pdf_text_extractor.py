124299a79db155eb06cf8bcbc1293fd83d1961cf
Sabin Ranabhat <binranabhat@gmail.com>
2026-03-10 12:36:46 +0545

    feat: implement text extractor from pdf for both text and image based pdfs

---
+++ app/pdf_text_extractor.py
@@ -0,0 +1,29 @@
+from pdfminer.high_level import extract_text
+from pdf2image import convert_from_path
+import pytesseract
+
+
+def extract_text_pdfmine(pdf_path: str) -> str:
+    return extract_text(pdf_path)
+
+
+def extract_text_ocr(pdf_path: str) -> str:
+    pages = convert_from_path(pdf_path)
+
+    full_text = ""
+
+    for page in pages:
+        text = pytesseract.image_to_string(page)
+        full_text += text + "\n"
+
+    return full_text
+
+
+def extract_text_from_pdf(pdf_path: str) -> str:
+
+    text = extract_text_pdfmine(pdf_path)
+
+    if len(text.strip()) < 100:
+        text = extract_text_ocr(pdf_path)
+
+    return text
